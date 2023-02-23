0c617bf551670631eecb195b83ed82b62eaca16c
Thenshy0 <torok.mercedesz13@gmail.com>
2023-02-23 11:35:16 +0100

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
+import React from "react";
 
 const Footer = () => {
-    return <footer>
-        foot
-    </footer>
-}
+  return <footer>foot TEST</footer>;
+};
 
 export default Footer;
