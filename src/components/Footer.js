9f84e9ab2d125b544c7c434402590ec1f05a33ac
Thenshy0 <torok.mercedesz13@gmail.com>
2023-02-25 14:06:58 +0100

    footer pics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,48 +9,76 @@ const Footer = () => {
           <div className="footer__colm">
             <h3>PRODUCTS</h3>
             <span className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/RFB6GSQY/Screenshot-47.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">Golden Rings</h3>
+                <p>$45.80</p>
               </span>
             </span>
             <span className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/QM5Kp955/Screenshot-48.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">
+                  Rose Gold Sapphire Ring
+                </h3>
+                <p>$21.20</p>
               </span>
             </span>
             <div className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/htXdt3t2/Screenshot-49.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">In the Air</h3>
+                <p>$19.99</p>
               </span>
             </div>
           </div>
           <div className="footer__colm">
             <h3>PRODUCTS</h3>
             <span className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/3xzD4CyY/Screenshot-50.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">Beautiful shoes</h3>
+                <p>$18.99</p>
               </span>
             </span>
             <span className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/xTxmYSnk/Screenshot-51.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">Blue Shirt</h3>
+                <p>$7.85</p>
               </span>
             </span>
             <div className="colm__row">
-              <img src="2.jpg" alt="" className="row__img" />
+              <img
+                src="https://i.postimg.cc/gkkhf0FF/Screenshot-52.png"
+                alt=""
+                className="row__img"
+              />
               <span className="row__details">
-                <h3>Title</h3>
-                <p>Price</p>
+                <h3 className="footer_product_title">
+                  Japanese Lucky Cat T-Shirt
+                </h3>
+                <p>$12.89</p>
               </span>
             </div>
           </div>
