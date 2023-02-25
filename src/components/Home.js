1672b2b5a18bb32595fe202d1dd7c5d40c0ff2e9
Thenshy0 <torok.mercedesz13@gmail.com>
2023-02-25 16:40:46 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import Slider from './Slider';
-import Products from './Products';
-
+import React from "react";
+import Header from "./Header";
+import Slider from "./Slider";
+import Products from "./Products";
 
 const Home = (data) => {
-    return (
-        <div>
-            <Header />
-            <Slider />
-            {data.isLoading && <p>the products are loading...</p>}
-            {data.error ? <p>{data.error}</p> : <Products products={data.products} cartItem={data.cartItem} />}
-            
-        </div>
-        )
-}
+  return (
+    <div>
+      <Header />
+      {/* <Slider /> */}
+      {data.isLoading && <p>the products are loading...</p>}
+      {data.error ? (
+        <p>{data.error}</p>
+      ) : (
+        <Products products={data.products} cartItem={data.cartItem} />
+      )}
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { TbShoppingCart } from "react-icons/tb";
 import { TbUser } from "react-icons/tb";
 import { IconContext } from "react-icons";
 
 const Navbar = ({ items }) => {
-    let sum = 0;
-    items.map((item) => { return sum += item.price });
-    return (
-        <div className="navbar">
-            <div className="navbar__top">
-                <h1>Company Name</h1>
-                <div className="navbar__topDetails">
-                    <IconContext.Provider value={{ className: "nav__icon1" }}>
-                        <TbUser />
-                    </IconContext.Provider>  
-                    <Link to='/cart' src='/'>     
-                        <span className="navbar__cartDetails">
-                            <IconContext.Provider value={{ className:"nav__icon" }}>
-                              <TbShoppingCart />
-                            </IconContext.Provider>                       
-                            <span>
-                                <h4>{items.length}Iterm</h4>
-                                <h3>${sum}</h3>
-                            </span>                       
-                        </span>
-                    </Link>
-                </div>
-            </div>
-          <nav>
-            <ul className="nav__links">
-              <li className='nav__link'><Link to='/'>Home</Link></li>
-              <li className='nav__link'><Link to='/'>Contact</Link></li>
-              <li className='nav__link'><Link to='/'>Register</Link></li>
-            </ul>
-            </nav>
+  let sum = 0;
+  items.map((item) => {
+    return (sum += item.price);
+  });
+  return (
+    <div className="navbar">
+      <div className="navbar__top">
+        <h1>Amaze Fashion</h1>
+        <div className="navbar__topDetails">
+          <IconContext.Provider value={{ className: "nav__icon1" }}>
+            <TbUser />
+          </IconContext.Provider>
+          <Link id="cart__link" to="/cart" src="/">
+            <span className="navbar__cartDetails">
+              <IconContext.Provider value={{ className: "nav__icon" }}>
+                <TbShoppingCart />
+              </IconContext.Provider>
+              <span>
+                <h4>{items.length}Iterm</h4>
+                <h3>${sum}</h3>
+              </span>
+            </span>
+          </Link>
         </div>
-    );
-  };
+      </div>
+      <nav>
+        <ul className="nav__links">
+          <li className="nav__link">
+            <Link id="nav__link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav__link">
+            <Link id="nav__link" to="/">
+              Contact
+            </Link>
+          </li>
+          <li className="nav__link">
+            <Link id="nav__link" to="/">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-
-
-  export default Navbar;
+export default Navbar;
